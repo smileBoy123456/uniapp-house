f2b355fa96a60957ce184ba4b1de3e2ebcf50b61
詹Sir <1099264057@qq.com>
2021-11-09 06:15:28 +0000

    换成自己的

--- RuoYi/ruoyi-common/src/main/java/com/ruoyi/common/utils/sms/DySmsEnum.java
+++ RuoYi/ruoyi-common/src/main/java/com/ruoyi/common/utils/sms/DySmsEnum.java
@@ -4,11 +4,11 @@ import com.ruoyi.common.utils.StringUtils;
 
 public enum DySmsEnum {
 
-	// 登录验证码
+	// 换成自己的登录验证码
 	LOGIN_TEMPLATE_CODE("***","南昌***有限公司","code"),
-	// 修改密码验证码
+	// 换成自己的修改密码验证码
 	FORGET_PASSWORD_TEMPLATE_CODE("***","南昌***有限公司","code"),
-	// 注册验证码
+	// 换成自己的注册验证码
 	REGISTER_TEMPLATE_CODE("***","南昌***有限公司","code");
 
 	/**
